0678d391585e542fabf0e73629fe1aec09f9f1d8
Gadi Paz <gadi.paz@gmail.com>
2016-12-08 00:09:37 +0200

    Lecture 14

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
-const App = function(){
-    return <div>Hi!</div>
+const API_KEY ='AIzaSyBUe6WTD19Q3QUqHSbXqh1jFs4pXV688qI';
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar/>
+        </div>
+    );
 }
 
-ReactDOM.render(App);
+ReactDOM.render(<App/>, document.querySelector('.container'));
