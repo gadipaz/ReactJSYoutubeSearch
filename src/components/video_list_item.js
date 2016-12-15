087cfdf0ca1545192d20a184a7a066ebd0a73a2c
Gadi Paz <gadi.paz@gmail.com>
2016-12-16 00:55:57 +0200

    Lecture 33 (Before Redux)

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,3 +1,19 @@
-/**
- * Created by GadiPaz on 07/12/2016.
- */
+import React from 'react';
+
+const VideoListItem = ({video, onVideoSelect}) =>{
+    const imageUrl = video.snippet.thumbnails.default.url;
+
+    return (
+        <li onClick={() => onVideoSelect(video)} className="list-group-item">
+            <div className="video-list media">
+                <div className="media-left">
+                    <img className="media-object" src={imageUrl}/>
+                </div>
+                <div className="media-body">
+                    <div className="media-heading">{video.snippet.title}</div>
+                </div>
+            </div>
+        </li>);
+};
+
+export default VideoListItem;
